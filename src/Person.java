d05659b4762be3c57e62478a546deb9a062d0528
Dan H <dan@hlud.net>
2019-06-07 16:33:19 -0400

    Added main class

--- src/Person.java
+++ src/Person.java
@@ -4,9 +4,10 @@ public class Person {
 	private String firstName;
 	private String lastName;
 	private String ssNumber;
+	private String phoneNumber;
 	
 	// Comment Please some more comments
-	
+	// Comre comments 
 	
 	
 }
